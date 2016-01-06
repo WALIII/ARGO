d930f82a585eb325b34a417560b3a2b7b31aba11
WALIII <bliberti@bu.edu>
2016-01-06 00:15:54 -0500

    update profiler

--- ARGO_profiler/argo_profiler.m
+++ ARGO_profiler/argo_profiler.m
@@ -1,23 +1,37 @@
 
+function ARGO_profiler
+% Get Info
+TEXT_DIR = '/Users/ARGO/Documents/MATLAB/ARGO/ARGO_profiler/INPUT.txt';
+INPUT = tdfread(TEXT_DIR,'\t'); % Assuimg that the .text file is in the path...
+BOX_ID = cellstr(INPUT.BOX_ID);
+STATUS = INPUT.STATUS;
+BIRD_ID = cellstr(INPUT.BIRD_ID);
+ENTRY_ID = cellstr(INPUT.ENTRY);
 
-G = 'BOX01_';
-GG = datestr(datetime('today'));
-GGG = '.txt';
-s = strcat(G,GG,GGG);
-filename = s;
-fileID = fopen(s,'w');
-counter = 1;
 
-space = '';
-BOXES = 'BOX01'
-BIRDS = 'BIRD01'
-COMMUTATORS= 'DPL01'
-ENTRY = GG
-LINE = 'DATE	       	HEALTH CHECK	        	SOCIAL TIME'
-DATA = GG
 
-fprintf(fileID,'%s\n','Name2')
-fprintf(fileID,'%s\n','EMAIL')
-fprintf(fileID,'%s\n',space)
-fprintf(fileID,'%s\n',LINE)
-fprintf(fileID,'%s\n',DATA)
+% Write Text File
+for i=1:length(BOX_ID)
+  if STATUS(i) == 1;
+    filename = char(strcat(BOX_ID(i),'_LOGFILE','.txt'));
+
+    fileID = fopen(filename,'w');
+    % Variable Names:
+    space = ' ';
+    BOXES = char(strcat('BOX ID:      ', BOX_ID(i)));
+    BIRDS = char(strcat('BIRD ID:     ', BIRD_ID(i)));
+    COMMUTATORS= char(strcat('COMMUTATOR:','DPL01'));
+    ENTRY = char(strcat('ENTRY DATE:', ENTRY_ID(i)));
+    LINE = 'DATE	       	HEALTH CHECK	        	SOCIAL TIME';
+    DATA = datestr(datetime('today'));
+% Write Data:
+fprintf(fileID,'%s\n',BOXES);
+fprintf(fileID,'%s\n',BIRDS);
+fprintf(fileID,'%s\n',ENTRY);
+fprintf(fileID,'%s\n',COMMUTATORS);
+fprintf(fileID,'%s\n',space);
+fprintf(fileID,'%s\n',LINE);
+fprintf(fileID,'%s\n',DATA);
+end
+end
+end
