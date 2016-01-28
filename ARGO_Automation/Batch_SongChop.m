0a3312ad5d640284637ab3ab4255d662aabf9f84
WALIII <bliberti@bu.edu>
2016-01-28 18:46:37 -0500

    repair automator

--- ARGO_Automation/Batch_SongChop.m
+++ ARGO_Automation/Batch_SongChop.m
@@ -6,27 +6,44 @@ function Batch_SongChop(DIR, varargin)
 
 
 
-counter = 1;
-
-if exist(Processed,'dir') rmdir(Processed,'s'); end
-mkdir(mat_dir);
 
 
-outlier_flag=0;
-if nargin<1 | isempty(DIR), DIR=pwd; end
-day_listing=dir(fullfile(DIR,'*.mat'));
-day_listing={day_listing(:).name};
-filenames=day_listing;
-
-
-disp('Creating Dff movies');
-
-[nblanks formatstring]=fb_progressbar(100);
-fprintf(1,['Progress:  ' blanks(nblanks)]);
-
-for i=1:length(day_listing)
-
-zftftb_song_chop(filenames{i});
-
+% if exist(Processed,'dir') rmdir(Processed,'s'); end
+% mkdir(mat_dir);
+here = pwd;
+% Get a list of all files and folders in this folder.
+files = dir(here);
+% Get a logical vector that tells which is a directory.
+dirFlags = [files.isdir];
+% Extract only those that are directories.
+subFolders = files(dirFlags);
+% Print folder names to command window.
+for k = 1 : length(subFolders)
+	zftftb_song_chop(subFolders(k).name);
+    delete('*.wav')
+    cd(here);
+    disp(k)
 end
-fprintf(1,'\n');
+
+
+% 
+% 
+% 
+% outlier_flag=0;
+% if nargin<1 | isempty(DIR), DIR=pwd; end
+% day_listing=dir(fullfile(DIR,'*.'));
+% day_listing={day_listing(:).name};
+% filenames=day_listing;
+% 
+% 
+% disp('Creating Dff movies');
+% 
+% [nblanks formatstring]=fb_progressbar(100);
+% fprintf(1,['Progress:  ' blanks(nblanks)]);
+% 
+% for i=1:length(day_listing)
+% 
+% zftftb_song_chop(filenames{i});
+% 
+% end
+% fprintf(1,'\n');
