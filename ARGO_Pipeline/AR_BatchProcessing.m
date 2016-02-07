ef1dfda61f86fb9654411b6152b7c6751c9f4a9d
WALIII <bliberti@bu.edu>
2016-02-07 10:29:23 -0500

    update new automation tools

---
+++ ARGO_Pipeline/AR_BatchProcessing.m
@@ -0,0 +1,4 @@
+function AR_BatchProcessing(function,level,scope)
+% AR_BatchProcessing(function) put the (function) that you want to run across all directories
+% Example: AR_BatchProcessing(AR_BatchDff_movfile,unprocessed,all_birds,today)
+% this will run batch processing on all birds, every day.
--- ARGO_Pipeline/AR_Batch_SongChop.m
+++ ARGO_Pipeline/AR_Batch_SongChop.m
@@ -1,4 +1,4 @@
-function Batch_SongChop(DIR, varargin)
+function AR_Batch_SongChop(DIR, varargin)
 
 % run thorough directory and song chop all the .wav files
 % WALIII
@@ -27,24 +27,24 @@ for k = 3 : length(subFolders)
 end
 
 
-% 
-% 
-% 
+%
+%
+%
 % outlier_flag=0;
 % if nargin<1 | isempty(DIR), DIR=pwd; end
 % day_listing=dir(fullfile(DIR,'*.'));
 % day_listing={day_listing(:).name};
 % filenames=day_listing;
-% 
-% 
+%
+%
 % disp('Creating Dff movies');
-% 
+%
 % [nblanks formatstring]=fb_progressbar(100);
 % fprintf(1,['Progress:  ' blanks(nblanks)]);
-% 
+%
 % for i=1:length(day_listing)
-% 
+%
 % zftftb_song_chop(filenames{i});
-% 
+%
 % end
 % fprintf(1,'\n');
